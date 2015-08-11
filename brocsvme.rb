aaeda32306c7272d1c35aa8b0567c907555d0063
Alex LaJeunesse <alexlajeunesse@gmail.com>
2015-08-11 14:32:20 -0700

    added flag for bad data

--- brocsvme.rb
+++ brocsvme.rb
@@ -8,20 +8,17 @@ require "optparse"
 row = []
 csv_array = []
 col_types = []
-special = [".", "<", ">", "/", "\\", "?", ";", "&", "%", "$", "@", "`", "(", ")", "*"]
-data_types = ["special", "string", "numeric", "float", "datetime", "time"]
+data_types = ["special", "alphanumeric", "numeric", "float", "datetime", "time"]
 
 options = {}
 optparse = OptionParser.new do |opts|
   opts.banner = "Use switches to customize the output csv."
 
-  opts.on("-r", "--rows [NUMROWS]",
-  "Number of rows in CSV") do |rows|
+  opts.on("-r", "--rows [NUMROWS]", "Number of rows in CSV") do |rows|
     options[:rows] = rows.to_i
   end
 
-  opts.on("-c", "--columns [NUMCOLS]",
-  "Number of columns in CSV") do |cols|
+  opts.on("-c", "--columns [NUMCOLS]", "Number of columns in CSV") do |cols|
     options[:columns] = cols.to_i
   end
 
@@ -29,9 +26,14 @@ optparse = OptionParser.new do |opts|
     options[:filename] = fname
   end
 
+  opts.on("-b", "--baddaata", "Bad data") do |bdata|
+    options[:baddata] = bdata
+  end
 end
 
 optparse.parse!
+
+options[:baddata] |= false
 
 output_file = options[:filename]
 output_file ||= "default_name.csv"
@@ -42,47 +44,61 @@ num_rows ||= 20
 num_cols = options[:columns]
 num_cols ||= data_types.length
 
+# oset = options[:baddaata] ? 1 : 0 # Offset for sample(rand()) calls, let 0 be valid for bad data.
+oset = if options[:baddata] then 0 else 1 end
+alphanumeric = [*'a'..'z', *'0'..'9']
+numeric = [*'0'..'9']
+special = [".", "<", ">", "/", "\\", "?", ";", "&", "%", "$", "@", "`", "(", ")", "*"]
+month = options[:baddaata] ? [*'0'..'30'] : [*'1'..'12']
+day = options[:baddata] ? [*'0'..'50'] : [*'1'..'28']
+year = options[:baddata] ? [*'1000'..'3000'] : [*'1980'..'2050']
+hour = options[:baddata] ? [*'0'..'50'] : [*'0'..'23']
+minute = options[:baddata] ? [*'0'..'100'] : [*'0'..'59']
+second = minute
+
 # Shuffle the column types
 for i in 0..(num_cols-1)
   col_types << data_types[i%7]
 end
 col_types = col_types.shuffle
+
 # Use the data type as the column headers
 csv << col_types.join(",") + "\n"
 
-puts "Creating CSV with #{num_rows} rows of columns with type\n\n#{col_types}"
+puts "Creating CSV with #{num_rows} rows of columns of type:\n#{col_types}"
 
 for i in 1..num_rows
+  print "\rWriting row #{i}."
+
   for type in col_types
     # Make a shuffled string of the desired data
-    if type == "string"
-      row << [*'a'..'z', *'0'..'9'].sample(rand(30)).join
+    if type == "alphanumeric"
+      row << alphanumeric.sample(rand(30)+oset).join
     elsif type == "numeric"
-      row << [*"0".."9"].sample(rand(26)).join
+      row << numeric.sample(rand(26)+oset).join
     elsif type == "float"
-      row << [*"0".."9"].sample(rand(10)).join +
-        "." + [*"0".."9"].sample(rand(6)).join
+      row << numeric.sample(rand(10)+oset).join +
+        "." + numeric.sample(rand(6)+oset).join
     elsif type == "date"
-      row << [*"1".."12"].sample + "-" +
-        [*"1".."31"].sample + "-" +
-        [*"1980".."2050"].sample
+      row << month.sample + "-" + day.sample + "-" + year.sample
     elsif type == "time"
-      row << "\"" + [*"0".."23"].sample.rjust(2, '0') + ":" +
-        [*"0".."59"].sample.rjust(2, '0') + ":" +
-        [*"0".."59"].sample.rjust(2, '0') + "\""
+      row << hour.sample.rjust(2, '0') + ":" +
+        minute.sample.rjust(2, '0') + ":" +
+        second.sample.rjust(2, '0')
     elsif type == "datetime"
-      row << [*"1980".."2050"].sample + "-" +
-        [*"1".."12"].sample.rjust(2, '0') + "-" +
-        [*"1".."29"].sample.rjust(2, '0') + "T" +
-        [*"0".."23"].sample.rjust(2, '0') + ":" +
-        [*"0".."59"].sample.rjust(2, '0') + ":" +
-        [*"0".."59"].sample.rjust(2, '0') + "-" +
-        [*"0".."23"].sample.rjust(2, '0') + ":" +
-        [*"0".."59"].sample.rjust(2, '0')
+      row << year.sample + "-" +
+        month.sample.rjust(2, '0') + "-" +
+        day.sample.rjust(2, '0') + "T" +
+        hour.sample.rjust(2, '0') + ":" +
+        minute.sample.rjust(2, '0') + ":" +
+        second.sample.rjust(2, '0') + "-" +
+        hour.sample.rjust(2, '0') + ":" +
+        minute.sample.rjust(2, '0')
     elsif type == "special"
-      row << "\"" + special.sample(rand(30)).join + "\""
+      row << "\"" + special.sample(rand(30)+oset).join + "\""
     end
   end
+
   # Add the string to the file as a row of the csv
   csv << row.join(",") + "\n"
   row = []
@@ -90,4 +106,4 @@ end
 
 csv.close
 
-puts "\n#{output_file} has been written.\n"
+puts "\rThe file #{output_file} has been written."
