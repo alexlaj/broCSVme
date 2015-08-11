3cb94202e78eaec403246f0fc1d180e214258279
Alex LaJeunesse <alexlajeunesse@gmail.com>
2015-08-11 13:16:32 -0700

    tweaking how data is selected

--- brocsvme.rb
+++ brocsvme.rb
@@ -9,20 +9,20 @@ row = []
 csv_array = []
 col_types = []
 special = [".", "<", ">", "/", "\\", "?", ";", "&", "%", "$", "@", "`", "(", ")", "*"]
-data_types = ["special", "string", "numeric", "financial", "date", "time", "datetime"]
+data_types = ["special", "string", "numeric", "float", "datetime", "time"]
 
 options = {}
 optparse = OptionParser.new do |opts|
   opts.banner = "Use switches to customize the output csv."
-  
-  opts.on("-r", "--rows [NUMROWS]", 
-          "Number of rows in CSV") do |rows|
+
+  opts.on("-r", "--rows [NUMROWS]",
+  "Number of rows in CSV") do |rows|
     options[:rows] = rows.to_i
   end
 
-  opts.on("-c", "--columns [NUMCOLS]", 
-          "Number of columns in CSV") do |cols|
-    options[:columns] = cols.to_i 
+  opts.on("-c", "--columns [NUMCOLS]",
+  "Number of columns in CSV") do |cols|
+    options[:columns] = cols.to_i
   end
 
   opts.on("-n", "--name [FILENAME]", "File name") do |fname|
@@ -40,7 +40,7 @@ csv = File.open(output_file, "w" )
 num_rows = options[:rows]
 num_rows ||= 20
 num_cols = options[:columns]
-num_cols ||= 7
+num_cols ||= data_types.length
 
 # Shuffle the column types
 for i in 0..(num_cols-1)
@@ -56,32 +56,31 @@ for i in 1..num_rows
   for type in col_types
     # Make a shuffled string of the desired data
     if type == "string"
-      row << (("a".."z").to_a + 
-              ("0".."9").to_a).shuffle[0,rand(30)].join
+      row << [*'a'..'z', *'0'..'9'].sample(rand(30)).join
     elsif type == "numeric"
-      row << ("0".."9").to_a.shuffle[0,rand(26)].join
-    elsif type == "financial"
-      row << "$" + ("0".."9").to_a.shuffle[0,rand(10)].join +
-             "." + ("0".."9").to_a.shuffle[0,rand(6)].join
+      row << [*"0".."9"].sample(rand(26)).join
+    elsif type == "float"
+      row << [*"0".."9"].sample(rand(10)).join +
+        "." + [*"0".."9"].sample(rand(6)).join
     elsif type == "date"
-      row << ("1".."12").to_a.shuffle[1] + "-" +
-             ("1".."31").to_a.shuffle[1] + "-" +
-             ("1980".."2050").to_a.shuffle[1]
+      row << [*"1".."12"].sample + "-" +
+        [*"1".."31"].sample + "-" +
+        [*"1980".."2050"].sample
     elsif type == "time"
-      row << ("0".."23").to_a.shuffle[1].rjust(2, '0') + ":" +
-             ("0".."59").to_a.shuffle[1].rjust(2, '0') + ":" + 
-             ("0".."59").to_a.shuffle[1].rjust(2, '0')
+      row << "\"" + [*"0".."23"].sample.rjust(2, '0') + ":" +
+        [*"0".."59"].sample.rjust(2, '0') + ":" +
+        [*"0".."59"].sample.rjust(2, '0') + "\""
     elsif type == "datetime"
-      row << ("1980".."2050").to_a.shuffle[1] + "-" +
-             ("1".."12").to_a.shuffle[1].rjust(2, '0') + "-" +
-             ("1".."31").to_a.shuffle[1].rjust(2, '0') + "T" +  
-             ("0".."23").to_a.shuffle[1].rjust(2, '0') + ":" +
-             ("0".."59").to_a.shuffle[1].rjust(2, '0') + ":" + 
-             ("0".."59").to_a.shuffle[1].rjust(2, '0') + "-" +
-             ("0".."23").to_a.shuffle[1].rjust(2, '0') + ":" +
-             ("0".."59").to_a.shuffle[1].rjust(2, '0')
+      row << [*"1980".."2050"].sample + "-" +
+        [*"1".."12"].sample.rjust(2, '0') + "-" +
+        [*"1".."29"].sample.rjust(2, '0') + "T" +
+        [*"0".."23"].sample.rjust(2, '0') + ":" +
+        [*"0".."59"].sample.rjust(2, '0') + ":" +
+        [*"0".."59"].sample.rjust(2, '0') + "-" +
+        [*"0".."23"].sample.rjust(2, '0') + ":" +
+        [*"0".."59"].sample.rjust(2, '0')
     elsif type == "special"
-      row << "\"" + special.to_a.shuffle[0,rand(20)].join + "\""
+      row << "\"" + special.sample(rand(30)).join + "\""
     end
   end
   # Add the string to the file as a row of the csv
